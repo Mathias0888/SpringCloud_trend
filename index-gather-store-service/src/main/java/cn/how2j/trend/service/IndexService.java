355b0777aab37d97f22624d36d886d65dd52ee70
liudi <liudi1120083004@icloud.com>
2022-10-18 11:04:02 +0800

    新增采集和存储微服务（刷新）

--- index-gather-store-service/src/main/java/cn/how2j/trend/service/IndexService.java
+++ index-gather-store-service/src/main/java/cn/how2j/trend/service/IndexService.java
@@ -1,10 +1,13 @@
 package cn.how2j.trend.service;
 
 import cn.how2j.trend.pojo.Index;
+import cn.how2j.trend.util.SpringContextUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.collection.CollectionUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,19 +22,39 @@ public class IndexService {
     private List<Index> indexes;
     @Autowired
     RestTemplate restTemplate;
+
+//    @HystrixCommand(fallbackMethod = "third_part_not_connected")
+//    @Cacheable(key = "'all_codes'")
+//    public List<Index> fetch_indexes_from_third_part(){
+//        List<Map> temp = restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json",List.class);
+//        return map2Index(temp);
+//    }
+
     @HystrixCommand(fallbackMethod = "third_part_not_connected")
+    public List<Index> fresh(){
+      indexes = fetch_indexes_from_third_part();
+      IndexService indexService = SpringContextUtil.getBean(IndexService.class);
+      indexService.remove();
+      return indexService.store();
+    };
+
+    @CacheEvict(allEntries = true)
+    public void remove(){
+
+    }
     @Cacheable(key = "'all_codes'")
+    public List<Index> store(){
+        System.out.println(this);
+        return indexes;
+    }
+    @Cacheable(key = "'all_codes'")
+    public List<Index> get(){
+        return CollUtil.toList();
+    }
+
     public List<Index> fetch_indexes_from_third_part(){
         List<Map> temp = restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json",List.class);
         return map2Index(temp);
-    }
-
-    public List<Index> third_part_not_connected(){
-        System.out.println("third_part_not_connected()");
-        Index index = new Index();
-        index.setCode("000000");
-        index.setName("无效的指数代码");
-        return CollectionUtil.toList(index);
     }
 
     private List<Index> map2Index(List<Map> temp){
@@ -45,5 +68,13 @@ public class IndexService {
             indexes.add(index);
         }
         return indexes;
+    }
+
+    public List<Index> third_part_not_connected(){
+        System.out.println("third_part_not_connected()");
+        Index index = new Index();
+        index.setCode("000000");
+        index.setName("无效的指数代码");
+        return CollectionUtil.toList(index);
     }
 }
--- index-gather-store-service/src/main/java/cn/how2j/trend/web/IndexController.java
+++ index-gather-store-service/src/main/java/cn/how2j/trend/web/IndexController.java
@@ -12,8 +12,22 @@ import java.util.List;
 public class IndexController {
     @Autowired
     IndexService indexService;
+
+//    @GetMapping("/getCodes")
+//    public List<Index> get() throws Exception{
+//        return indexService.fetch_indexes_from_third_part();
+//    }
+    @GetMapping("/freshCodes")
+    public List<Index> fresh() throws Exception{
+        return indexService.fresh();
+    }
     @GetMapping("/getCodes")
     public List<Index> get() throws Exception{
-        return indexService.fetch_indexes_from_third_part();
+        return indexService.get();
+    }
+    @GetMapping("/removeCodes")
+    public String remove() throws Exception{
+        indexService.remove();
+        return "remove codes successfully";
     }
 }
