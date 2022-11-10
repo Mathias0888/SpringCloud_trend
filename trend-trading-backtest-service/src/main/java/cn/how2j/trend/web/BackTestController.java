7fe9b46500acd67ba2f38600a65d32b233d236ff
liudi <liudi1120083004@icloud.com>
2022-11-10 10:07:02 +0800

    新增模拟回测-MA均线

--- trend-trading-backtest-service/src/main/java/cn/how2j/trend/web/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/cn/how2j/trend/web/BackTestController.java
@@ -20,9 +20,10 @@ public class BackTestController {
     @Autowired
     BackTestService backTestService;
 
-    @GetMapping("/simulate/{code}/{startDate}/{endDate}")
+    @GetMapping("/simulate/{code}/{ma}/{startDate}/{endDate}")
     @CrossOrigin
     public Map<String,Object> backTest(@PathVariable("code")String code
+            ,@PathVariable("ma")int ma
             ,@PathVariable("startDate")String strStartDate
             ,@PathVariable("endDate")String strEndDate)throws Exception{
 
@@ -33,7 +34,6 @@ public class BackTestController {
 
         allIndexDatas = filterByDateRange(allIndexDatas,strStartDate,strEndDate);
 
-        int ma = 20;
         float sellRate = 0.95f;
         float buyRate = 1.05f;
         float serviceCharge = 0f;
